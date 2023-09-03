ec240e2ba1d19c33fc1d45b54b5eaf99ea769e73
sloth <siddharthn02@gmail.com>
2023-09-03 13:44:16 +0530

    final changes

--- Q1/api/schedule.py
+++ Q1/api/schedule.py
@@ -10,6 +10,7 @@ ACCESS_TOKEN = {
     'access_token': (None, None)
 }
 
+
 class CurrentTrainAPI(Resource):
     """
     Route: /api/trains
@@ -19,28 +20,28 @@ class CurrentTrainAPI(Resource):
         access_token = ACCESS_TOKEN.get('access_token')
         print(access_token)
         if access_token[0] is None:
-            res = requests.post(JOHN_DOE_BASE + '/train/auth',json={
-                "companyname":os.getenv('companyname'),
-                "clientID":os.getenv('clientID'),
-                "clientSecret":os.getenv('clientSecret'),
-                "ownerEmail":os.getenv('ownerEmail'),
-                "ownerName":os.getenv('ownerName'),
-                "rollNo":os.getenv('rollNo')
+            res = requests.post(JOHN_DOE_BASE + '/train/auth', json={
+                "companyname": os.getenv('companyname'),
+                "clientID": os.getenv('clientID'),
+                "clientSecret": os.getenv('clientSecret'),
+                "ownerEmail": os.getenv('ownerEmail'),
+                "ownerName": os.getenv('ownerName'),
+                "rollNo": os.getenv('rollNo')
             })
             if res.status_code != 200:
-                print('jehr',res.status_code)
+                print('jehr', res.status_code)
                 return {'error': 'John Doe server is down'}, 500
             ACCESS_TOKEN['token'] = res.json()['access_token']
             access_token = ACCESS_TOKEN.get('access_token')[0]
 
         elif access_token[1] < datetime.datetime.now().timestamp():
-            res = requests.post(JOHN_DOE_BASE + '/train/auth',json={
-                "companyname":os.getenv('companyname'),
-                "clientID":os.getenv('clientID'),
-                "clientSecret":os.getenv('clientSecret'),
-                "ownerEmail":os.getenv('ownerEmail'),
-                "ownerName":os.getenv('ownerName'),
-                "rollNo":os.getenv('rollNo')
+            res = requests.post(JOHN_DOE_BASE + '/train/auth', json={
+                "companyname": os.getenv('companyname'),
+                "clientID": os.getenv('clientID'),
+                "clientSecret": os.getenv('clientSecret'),
+                "ownerEmail": os.getenv('ownerEmail'),
+                "ownerName": os.getenv('ownerName'),
+                "rollNo": os.getenv('rollNo')
             })
             if res.status_code != 200:
                 print(res.status_code)
@@ -62,8 +63,9 @@ class CurrentTrainAPI(Resource):
         for train in trains:
             train['departureTime'] = datetime.datetime(year=now.year,
                                                        month=now.month,
-                                                         day=now.day,
-                                                       hour=int(train['departureTime']['Hours']),
+                                                       day=now.day,
+                                                       hour=int(
+                                                           train['departureTime']['Hours']),
                                                        minute=int(
                                                            train['departureTime']['Minutes']),
                                                        second=int(
@@ -73,17 +75,17 @@ class CurrentTrainAPI(Resource):
                 if ((train['departureTime'] - now).seconds//60//60) == 0 and ((train['departureTime'] - now).seconds//60) >= 30:
                     filtered_trains.append(train)
 
-                   
-
-
         # sort by price, then by seats available, then by departure time
         trains.sort(key=lambda x: (
-
             -x['departureTime'].timestamp(),
             -(x['seatsAvailable']['sleeper'] + x['seatsAvailable']['AC']),
             x['price']['sleeper']+x['price']['AC'],
         ))
-        return res.json(), 200
+
+        for train in trains:
+            train['departureTime'] = train['departureTime'].strftime(
+                '%Y-%m-%d %H:%M:%S')
+        return {'trains':trains}, 200
 
 
 class GetAccessToken(Resource):
@@ -92,16 +94,17 @@ class GetAccessToken(Resource):
     """
 
     def get(self):
-        res = requests.post(JOHN_DOE_BASE + '/train/auth',json={
-            "companyName":os.getenv('companyName'),
-            "clientID":os.getenv('clientID'),
-            "clientSecret":os.getenv('clientSecret'),
-            "ownerEmail":os.getenv('ownerEmail'),
-            "ownerName":os.getenv('ownerName'),
-            "rollNo":os.getenv('rollNo')
+        res = requests.post(JOHN_DOE_BASE + '/train/auth', json={
+            "companyName": os.getenv('companyName'),
+            "clientID": os.getenv('clientID'),
+            "clientSecret": os.getenv('clientSecret'),
+            "ownerEmail": os.getenv('ownerEmail'),
+            "ownerName": os.getenv('ownerName'),
+            "rollNo": os.getenv('rollNo')
         })
         if res.status_code != 200:
             print(res.status_code)
             return {'error': 'John Doe server is down'}, 500
-        ACCESS_TOKEN['access_token'] = (res.json()['access_token'],res.json()['expires_in'])
+        ACCESS_TOKEN['access_token'] = (
+            res.json()['access_token'], res.json()['expires_in'])
         return res.json(), 200
--- Q1/main.py
+++ Q1/main.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from flask_restful import Api
+from flask_cors import CORS
 import dotenv
 
 from api.schedule import *
 
 dotenv.load_dotenv()
 app = Flask(__name__)
+cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 api = Api(app)
 
 api.add_resource(CurrentTrainAPI, '/api/trains')
